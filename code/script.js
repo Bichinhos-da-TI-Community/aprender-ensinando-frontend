31da12771967aaf4a6381c3bb393782114c322c1
luizhf42 <luizhf42@gmail.com>
2023-12-27 14:19:36 -0300

    fix: "boton" to button

--- code/script.js
+++ code/script.js
@@ -1,7 +1,9 @@
 function toggleDarkMode() {
-    var body = document.body;
-    var botonImage = document.querySelector('.custom-boton img');
-  
-    body.classList.toggle("dark-mode");
-    botonImage.src = body.classList.contains('dark-mode') ? '../img/dark-mode-button.png' : '../img/light-mode-button.png';
-  }
+	var body = document.body;
+	var buttonImage = document.querySelector(".custom-button img");
+
+	body.classList.toggle("dark-mode");
+	buttonImage.src = body.classList.contains("dark-mode")
+		? "../img/dark-mode-button.png"
+		: "../img/light-mode-button.png";
+}
